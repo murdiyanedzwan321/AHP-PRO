9536bb4fe1e0ebdb36bcd986cbddcfaafc5ef8cc
Edzwan <murdiyanedzwan444@gmail.com>
2026-01-15 19:40:52 +0700

    kfkhusfsew

--- lib/services/ahp_service.dart
+++ lib/services/ahp_service.dart
@@ -6,7 +6,7 @@ class AhpService {
   // Di Emulator Android gunakan 10.0.2.2 sebagai localhost
   // Di Web/iOS gunakan 127.0.0.1 atau localhost
   // Ganti port sesuai Flask (5000)
-  static const String _fallbackUrl = 'http://127.0.0.1:5000'; 
+  static const String _fallbackUrl = 'https://gist.githubusercontent.com/murdiyanedzwan321/270d97517bef8531775145eecdda01f5/raw/d310af7b602fa2f8f0f145961bb18465102a9a5f/config.json'; 
   
   // URL Raw Gist (Nanti diisi User sesuai TOR)
   static const String _configGistUrl = 'https://gist.githubusercontent.com/murdiyanedzwan321/270d97517bef8531775145eecdda01f5/raw/d310af7b602fa2f8f0f145961bb18465102a9a5f/config.json';
